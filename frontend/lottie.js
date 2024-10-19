90efa9c1cfc2eb52978d918e6a44ec6d6210722d
connortbot <loiconnor8@gmail.com>
2024-10-19 15:11:20 -0400

    second pig

--- frontend/lottie.js
+++ frontend/lottie.js
@@ -2,6 +2,8 @@ import Lottie from "lottie-react";
 import animation from "./lottie-animations/pig-wheel.json";
 import first_pig_talking from "./lottie-animations/first-pig-talking.json";
 import first_pig_thinking from "./lottie-animations/first-pig-thinking.json";
+import second_pig_talking from "./lottie-animations/second-pig-talking.json";
+import second_pig_thinking from "./lottie-animations/second-pig-thinking.json";
 
 // EXAMPLE FOR LOADING LOTTIE ANIMS, RETURNS AS REACT COMPONENT
 
@@ -12,8 +14,11 @@ const Example = () => {
         <h3>First Pig</h3>
         <Lottie animationData={first_pig_talking} />;
         <Lottie animationData={first_pig_thinking} />;
+        <h3>Second Pig</h3>
+        <Lottie animationData={second_pig_talking} />;
+        <Lottie animationData={second_pig_thinking} />;
     </div>
     
 };
-  
+
 export default Example
