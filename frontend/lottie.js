ed06aeabcc0af1a2edb3c444f30edaa0adf81748
Pranav Bedi <pranav.bedi.work@gmail.com>
2024-10-18 23:48:49 -0400

    Initial Commit

---
+++ frontend/lottie.js
@@ -0,0 +1,10 @@
+import Lottie from "lottie-react";
+import animation from "./lottie-animations/pig-wheel.json";
+
+// EXAMPLE FOR LOADING LOTTIE ANIMS, RETURNS AS REACT COMPONENT
+
+const Example = () => {
+    return <Lottie animationData={animation} />;
+};
+  
+export default Example
