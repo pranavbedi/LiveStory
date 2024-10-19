ddfa754f653d770df1d39f5b6f234901ad61931f
connortbot <loiconnor8@gmail.com>
2024-10-19 14:44:17 -0400

    pig animations on lottie

--- frontend/lottie.js
+++ frontend/lottie.js
@@ -1,10 +1,19 @@
 import Lottie from "lottie-react";
 import animation from "./lottie-animations/pig-wheel.json";
+import first_pig_talking from "./lottie-animations/first-pig-talking.json";
+import first_pig_thinking from "./lottie-animations/first-pig-thinking.json";
 
 // EXAMPLE FOR LOADING LOTTIE ANIMS, RETURNS AS REACT COMPONENT
 
 const Example = () => {
-    return <Lottie animationData={animation} />;
+    return <div>
+        <h3>Pig Wheel</h3>
+        <Lottie animationData={animation} />;
+        <h3>First Pig</h3>
+        <Lottie animationData={first_pig_talking} />;
+        <Lottie animationData={first_pig_thinking} />;
+    </div>
+    
 };
   
 export default Example
