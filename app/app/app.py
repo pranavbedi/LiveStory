976c27019be75b38dba1b4a6c69f133ec546865b
Pranav Bedi <pranav.bedi.work@gmail.com>
2024-10-20 09:37:24 -0400

    Landing page includes navbar and images

--- app/app/app.py
+++ app/app/app.py
@@ -2,41 +2,104 @@ import reflex as rx
 from rxconfig import config
 from .pages.story_page import story
 from .getPages import get_page
+from .navBar import navbar_searchbar
 
 from .characterComponent import InteractableCharacter
 
 class State(rx.State):
-    """The app state."""
-    ...
+
+    talking: bool = False
+
+    def start_talking(self):
+        self.talking = True
+
+    def stop_talking(self):
+        self.talking = False
+
+
 
 
 def index() -> rx.Component:
     # Landing page
     return rx.vstack(
-        rx.color_mode.button(position="top-right"),
-        rx.text("Story Book",
-            font_size="5vh",
-            font_family="Comic Sans MS",
-            text_align="center",
-            padding="10px"
-        ),
-        rx.center(
+        navbar_searchbar(),
+        rx.hstack(
             rx.link(
                 rx.image(
-                    src="ThreeLittlePigs.png",
-                    width="60%",
-                    height="auto",
-                    margin="auto",
-                    border_radius="1.5em",
+                    src="ThreeLittlePigs.jpg",
+                    width="auto",
+                    height="200px",
+                    margin="5px",
+                    border = " 3px solid green",
+                    border_radius="10px 50px 50px 10px",
                 ),
                 href="/story",
             ),
-            width="100%",
+            rx.link(  # Place the block before the width argument
+                rx.image(
+                    src="WeAreAll.png",
+                    width="auto",
+                    height="200px",
+                    border = " 3px solid brown",
+                    margin="5px",
+                    border_radius="10px 50px 50px 10px",
+                ),
+                href="/404",
+            ),
+            rx.link(  # Place the block before the width argument
+                rx.image(
+                    src="muddle.jpg",
+                    width="auto",
+                    height="200px",
+                    border = " 3px solid purple",
+                    margin="5px",
+                    border_radius="10px 50px 50px 10px",
+                ),
+                href="/404",
+            ),
+            
+        ),
+        rx.hstack(
+            rx.link(
+                rx.image(
+                    src="caterpillar.png",
+                    width="auto",
+                    height="200px",
+                    margin="5px",
+                    border = " 3px solid red",
+                    border_radius="10px 50px 50px 10px",
+                ),
+                href="/404",
+            ),
+            rx.link(  # Place the block before the width argument
+                rx.image(
+                    src="ribbit.jpg",
+                    width="auto",
+                    height="200px",
+                    margin="5px",
+                    border = " 3px solid green",
+                    border_radius="10px 50px 50px 10px",
+                ),
+                href="/404",
+            ),
+            rx.link(  # Place the block before the width argument
+                rx.image(
+                    src="bear.jpeg",
+                    width="auto",
+                    height="200px",
+                    margin="5px",
+                    border = " 3px solid yellow",
+                    border_radius="10px 50px 50px 10px",
+                ),
+                href="/404",
+            ),
+            
         ),
         spacing="5",
         align="center",
         justify="center",
         height="100vh",
+        bg="beige",
     )
 
 app = rx.App()
